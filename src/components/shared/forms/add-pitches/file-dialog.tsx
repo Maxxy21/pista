8143d817e811722c58baf47a7ef8ae425ec02f50
Maxwell Aboagye <nanabaah12@gmail.com>
2026-06-13 01:01:43 +0200

    remove dead pitch dialog

--- src/components/shared/forms/add-pitches/file-dialog.tsx
+++
@@ -1,5 +0,0 @@
-// Legacy export for backwards compatibility
-// This file has been refactored into smaller components.
-// Use PitchDialog from ./pitch-dialog.tsx instead.
-
-export { PitchDialog as FileDialog } from "./pitch-dialog";
